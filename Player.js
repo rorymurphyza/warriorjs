6e4f891bcad6682b916af1c0584a1c1ca0a3c667
rorymurphyza <rory@rorym.co.za>
2016-10-14 16:00:45 +1100

    Level 4 Commit

--- Player.js
+++ Player.js
@@ -3,31 +3,47 @@ class Player
   constructor(warrior) 
   {
     this.MaxHealth = 20;
+    this.CurrentHealth = 20;
   }
 
   playTurn(warrior) {
-    //check health level to see what we should do
-    if (warrior.health() < 0.8 * this.MaxHealth)  //we are below 80% health
+    if (warrior.health() < this.CurrentHealth) //we have lost health since the last round
     {
-      if (warrior.feel().isEmpty()) //nothing ahead of us
-      {
-        warrior.rest();
-      }
-      else  //something is ahead of us
-      {
-        warrior.attack();
-      }
-    }
-    else //we have a high enough health to carry on
-    {
-      if (warrior.feel().isEmpty()) //nothing is ahead of us
+      if (warrior.feel().isEmpty()) //there is nothing ahead of us
       {
         warrior.walk();
       }
-      else  //something is ahead of us
+      else  //there is something ahead of us
       {
         warrior.attack();
       }
     }
+    else //we are not being attacked
+    {
+      //check health level to see what we should do
+      if (warrior.health() < 0.8 * this.MaxHealth)  //we are below 80% health
+      {
+        if (warrior.feel().isEmpty()) //nothing ahead of us
+        {
+          warrior.rest();
+        }
+        else  //something is ahead of us
+        {
+          warrior.attack();
+        }
+      }
+      else //we have a high enough health to carry on
+      {
+        if (warrior.feel().isEmpty()) //nothing is ahead of us
+        {
+          warrior.walk();
+        }
+        else  //something is ahead of us
+        {
+          warrior.attack();
+        }
+      }
+    }
+    this.CurrentHealth = warrior.health();
   }
 }
